673c77c8376701b6da52ad332c1f9f338cd28878
KrohnosHub <matthias.krohn@hotmail.com>
2026-09-23 23:41:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker der Gesangs-App – hält Programm und pdf.js offline bereit.
 // Blätter und Notizen selbst liegen in IndexedDB (nicht hier).
-const VERSION = '2.3.3-919d3374';
+const VERSION = '2.3.4-487256d2';
 const CACHE = 'gesangs-app-' + VERSION;
 const PRECACHE = [
  "./",
